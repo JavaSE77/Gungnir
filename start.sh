8c31ebcf778d702f0ea6228aa40e574203c6f378
Russell Stevens <rs0699@cs.ship.edu>
2022-04-07 15:22:06 -0400

    added -e

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lxterminal "pi4j --run -Xmx2000M -Xms200M -XX:PermSize=256m -XX:MaxPermSize=512m -jar server.jar"
+lxterminal -e "pi4j --run -Xmx2000M -Xms200M -XX:PermSize=256m -XX:MaxPermSize=512m -jar server.jar"
