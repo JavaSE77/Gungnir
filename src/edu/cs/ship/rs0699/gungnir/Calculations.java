870a6941b746aee8985d97cabe829e982e79fd6f
Russell Stevens <rs0699@cs.ship.edu>
2022-04-27 17:51:02 -0400

    I am so tired I don't understand how speed works any more

--- src/edu/cs/ship/rs0699/gungnir/Calculations.java
+++ src/edu/cs/ship/rs0699/gungnir/Calculations.java
@@ -30,7 +30,7 @@ public class Calculations {
       
       long timeDelta = sensorReadings[2] - sensorReadings[0];
       //Time to go 4 feet. / 4 to get the time to go 1 foot. Then * .68 to get mph
-      double speed = ((((distance * 2)) / (timeDelta))/4 * 0.681818);
+      double speed = (((distance * 2)/(timeDelta)) / 681.818);
       
       if( Main.verbose) System.out.println("Calculated speed: " + speed);
       return speed;
