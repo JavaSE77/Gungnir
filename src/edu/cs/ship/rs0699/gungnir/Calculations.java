9b70b6e6bde086f52689fe112f8f9afbf6ad5894
Russell Stevens <rs0699@cs.ship.edu>
2022-04-27 18:23:51 -0400

    Changed speed calculation

--- src/edu/cs/ship/rs0699/gungnir/Calculations.java
+++ src/edu/cs/ship/rs0699/gungnir/Calculations.java
@@ -31,7 +31,7 @@ public class Calculations {
       long timeDelta = sensorReadings[2] - sensorReadings[0];
       //Time to go 4 feet. / 4 to get the time to go 1 foot. Then * .682 to get mph
       System.out.println(timeDelta);//.392
-      double speed = (((distance * 2)*(1.0/(timeDelta / 1000))) * .682);
+      double speed = (((distance * 2)/((timeDelta / 1000))) * .682);
       
       if( Main.verbose) System.out.println("Calculated speed: " + speed);
       return speed;
