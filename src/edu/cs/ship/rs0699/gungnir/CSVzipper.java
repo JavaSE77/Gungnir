1585321b92a7f201ef49512099d1b1879c9702ab
Russell Stevens <rs0699@cs.ship.edu>
2022-05-02 16:58:40 -0400

    Setup zip downloading

--- src/edu/cs/ship/rs0699/gungnir/CSVzipper.java
+++ src/edu/cs/ship/rs0699/gungnir/CSVzipper.java
@@ -3,6 +3,9 @@ package edu.cs.ship.rs0699.gungnir;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -19,18 +22,21 @@ public class CSVzipper implements HttpHandler {
   public void handle(HttpExchange exchange) throws IOException {
     // TODO Auto-generated method stub
 
+    byte[] data = null;
     int responseCode = 400;
     try {
       CSV.addDirToZipArchive((new File("records/")), null);
+      Path path = Paths.get("archive.zip");
+      data = Files.readAllBytes(path);
       responseCode = 200;
     } catch (Exception e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
     String response = "Thank you for downloading our records.";
-    exchange.sendResponseHeaders(responseCode, response.length());
+    exchange.sendResponseHeaders(responseCode, data.length);
     OutputStream os = exchange.getResponseBody();
-    os.write(response.getBytes());
+    os.write(data);
     System.out.println(exchange.getRequestURI());
     os.close();
     
